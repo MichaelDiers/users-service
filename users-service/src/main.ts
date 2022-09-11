7cbdb1a79b9dae9b081567c8f51cfdf254e6df38
MichaelDiers <md@sreid.de>
2022-09-11 15:21:34 +0200

    create hashes of dto objects for its email and password

--- users-service/src/main.ts
+++ users-service/src/main.ts
@@ -1,10 +1,14 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HashPipe } from './pipes/hash-pipe';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({ stopAtFirstError: true }));
+  app.useGlobalPipes(
+    new ValidationPipe({ stopAtFirstError: true }),
+    new HashPipe(),
+  );
   await app.listen(3000);
 }
 
