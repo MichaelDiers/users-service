c5e991a874847b4c78be08f862efdfbcaa6a1dc9
MichaelDiers <md@sreid.de>
2022-09-15 00:24:37 +0200

    add configuration module for reading .env and secret manager values

---
+++ users-service/src/configuration/configuration.module.ts
@@ -0,0 +1,130 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { Transport } from '@nestjs/microservices';
+import { join } from 'path';
+import { EnvNames } from './env-names';
+import { ServicesModule } from '../services/services.module';
+import { InjectionNames } from './InjectionNames.enum';
+import { SecretManagerService } from './secret-manager.service';
+import { DocumentBuilder } from '@nestjs/swagger';
+import { HeaderNames } from '../header-names';
+
+@Module({
+  exports: [
+    InjectionNames.API_KEY,
+    InjectionNames.GRPC_CONFIG,
+    InjectionNames.TCP_CONFIG,
+    InjectionNames.USE_SWAGGER,
+    InjectionNames.HASH_ROUNDS,
+    InjectionNames.PROJECT_NAME,
+    InjectionNames.SECRETS_FROM_ENV,
+    InjectionNames.CONNECTION_STRING,
+    InjectionNames.SWAGGER_CONFIG,
+  ],
+  imports: [ConfigModule.forRoot({}), ServicesModule],
+  providers: [
+    SecretManagerService,
+    {
+      provide: InjectionNames.API_KEY,
+      useFactory: (configService: ConfigService): string => {
+        return configService.getOrThrow(EnvNames.API_KEY);
+      },
+      inject: [ConfigService],
+    },
+    {
+      provide: InjectionNames.CONNECTION_STRING,
+      useFactory: async (
+        secretsFromEnv: boolean,
+        configService: ConfigService,
+        secretManagerService: SecretManagerService,
+      ): Promise<string> => {
+        if (secretsFromEnv) {
+          return configService.getOrThrow(EnvNames.CONNECTION_STRING);
+        }
+
+        return secretManagerService.getConnectionString();
+      },
+      inject: [
+        InjectionNames.SECRETS_FROM_ENV,
+        ConfigService,
+        SecretManagerService,
+      ],
+    },
+    {
+      provide: InjectionNames.GRPC_CONFIG,
+      useFactory: (configService: ConfigService) => {
+        const port = configService.getOrThrow(EnvNames.GRPC_PORT);
+        return {
+          transport: Transport.GRPC,
+          options: {
+            package: ['users'],
+            protoPath: [join(__dirname, '../proto/users.proto')],
+            url: `localhost:${port}`,
+          },
+        };
+      },
+      inject: [ConfigService],
+    },
+    {
+      provide: InjectionNames.HASH_ROUNDS,
+      useFactory: (configService: ConfigService): number => {
+        const rounds = configService.getOrThrow(EnvNames.HASH_ROUNDS);
+        return parseInt(rounds);
+      },
+      inject: [ConfigService],
+    },
+    {
+      provide: InjectionNames.PROJECT_NAME,
+      useFactory: (configService: ConfigService): string => {
+        return configService.getOrThrow(EnvNames.PROJECT_NAME);
+      },
+      inject: [ConfigService],
+    },
+    {
+      provide: InjectionNames.SECRETS_FROM_ENV,
+      useFactory: (configService: ConfigService): boolean => {
+        const value = configService.getOrThrow(EnvNames.SECRETS_FROM_ENV);
+        return value ? true : false;
+      },
+      inject: [ConfigService],
+    },
+    {
+      provide: InjectionNames.SWAGGER_CONFIG,
+      useFactory: () => {
+        return new DocumentBuilder()
+          .setTitle('UsersService')
+          .setDescription('The api of the users service.')
+          .setVersion('1.0')
+          .addTag('users')
+          .addApiKey(
+            { type: 'apiKey', name: HeaderNames.X_API_KEY, in: 'header' },
+            HeaderNames.X_API_KEY,
+          )
+          .build();
+      },
+    },
+    {
+      provide: InjectionNames.TCP_CONFIG,
+      useFactory: (configService: ConfigService): any => {
+        const port = configService.getOrThrow(EnvNames.TCP_PORT);
+        return {
+          transport: Transport.TCP,
+          options: {
+            host: '0.0.0.0',
+            port,
+          },
+        };
+      },
+      inject: [ConfigService],
+    },
+    {
+      provide: InjectionNames.USE_SWAGGER,
+      useFactory: (configService: ConfigService): boolean => {
+        const value = configService.getOrThrow(EnvNames.USE_SWAGGER);
+        return value ? true : false;
+      },
+      inject: [ConfigService],
+    },
+  ],
+})
+export class ConfigurationModule {}
---
+++ users-service/src/configuration/secret-manager.service.ts
@@ -0,0 +1,65 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { SecretManagerServiceClient } from '@google-cloud/secret-manager';
+import { ISecretManagerService } from '../services/secret-manager.interface';
+import {
+  ILoggingService,
+  LOGGING_SERVICE,
+} from '../services/logging.interface';
+import { InjectionNames } from './InjectionNames.enum';
+
+/**
+ * The names of secrets.
+ */
+const enum SecretNames {
+  CONNECTION_STRING = 'UsersServiceConnectionString',
+}
+
+/**
+ * Access for the google cloud secret manager.
+ */
+@Injectable()
+export class SecretManagerService implements ISecretManagerService {
+  /**
+   * Creates a new SecretManagerService instance.
+   * @param loggingService An error logger.
+   */
+  constructor(
+    @Inject(LOGGING_SERVICE) private readonly loggingService: ILoggingService,
+    @Inject(InjectionNames.PROJECT_NAME) private readonly projectName: string,
+  ) {}
+
+  /**
+   * The client for accessing the google secret manager.
+   */
+  private client: SecretManagerServiceClient;
+
+  /**
+   * Gets the mongodb connection string.
+   * @returns The mongodb connection string if the secret exists and undefined otherwise.
+   */
+  async getConnectionString(): Promise<string | undefined> {
+    return this.getSecretAsync(SecretNames.CONNECTION_STRING);
+  }
+
+  /**
+   * Access the latest version of a secret.
+   * @param secretName The name of the secret.
+   * @returns The value of the latest version of the secret if the secret exists and undefined otherwise.
+   */
+  private async getSecretAsync(
+    secretName: string,
+  ): Promise<string | undefined> {
+    if (!this.client) {
+      this.client = new SecretManagerServiceClient();
+    }
+
+    const name = `projects/${this.projectName}/secrets/${secretName}/versions/latest`;
+    try {
+      const [version] = await this.client.accessSecretVersion({ name });
+      return version.payload.data.toString();
+    } catch (err) {
+      this.loggingService.logError(err.message, err.stack);
+      return;
+    }
+  }
+}
